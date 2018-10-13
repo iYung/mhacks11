88b479d55c462a90a6065fb3084f5694ffbf91f0
iYung <ivankhyung@gmail.com>
2018-10-13 19:30:19 -0400

    code clean

--- extension/background.js
+++
@@ -1,7 +0,0 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request && request.action === 'createTab') {
-      chrome.tabs.create({url: 'page/page.html'}, function (win) {
-        sendResponse(win);
-      });
-    }
-});
