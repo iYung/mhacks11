e1231688ceb0a91987576e9fd7513917ee4be9b2
iYung <ivankhyung@gmail.com>
2018-10-12 22:23:18 -0400

    added nlp to reddit

--- extension/main.js
+++ extension/main.js
@@ -1,19 +1,38 @@
 var sentence = "";
+var flag = false;
 
 document.addEventListener('click',function(e){
     if(e.target && e.target.innerHTML == 'save'){
-        if (isRude() && confirm("This is rude! Are you sure you want to post this?")) {
-            
-        } else {
-            alert(sentence);
-            e.preventDefault();
+      if (!flag) {
+        e.preventDefault();
+        var data = {
+          "document":{
+              "type":"PLAIN_TEXT",
+              "content": sentence
+          },
+          "encodingType": "UTF8"
         };
+        fetch("https://language.googleapis.com/v1/documents:analyzeSentiment?key=AIzaSyCSX9CVGcgKYIHw0uciv5IvAzB2i7iEeZk", {
+            method: "POST", 
+            body: JSON.stringify(data)
+        }).then(function(response) {
+          return response.json();
+        })
+        .then(function(res) {
+          console.log(res.documentSentiment.score)
+          if (res.documentSentiment.score < 0 && confirm("This is rude! Are you sure you want to post this?")) {
+            flag = true;
+            e.target.click();
+          } else if (res.documentSentiment.score >= 0) {
+            flag = true;
+            e.target.click();
+          }
+        })
+      } else {
+        flag = false
+      }
     }
 })
-
-function isRude() {
-    return true;
-}
 
 document.addEventListener('keyup', function(e){
     if (e.target && e.target.localName == 'textarea') {
